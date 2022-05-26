9ff5bdfa722450e0a2933c0c76ab5e023d35bf54
Vitali Aleksandruk <60776251+vVitalix@users.noreply.github.com>
2022-05-26 23:23:52 +0300

    first commit

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Navbar from "../components/Navbar";
+
+class Main extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className="container"></div>
+      </>
+    );
+  }
+}
+
+export default Main;
